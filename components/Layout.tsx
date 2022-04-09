94b8f659eb346913291ef1a69ebb2e5a0654578a
Matt Dyor <matt@dyor.com>
2022-04-09 12:43:40 -0700

    add google analytics

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,19 +1,9 @@
 import React, { ReactNode } from "react";
 import Header from "./Header";
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-
-
-// import ButtonsShowcase from './showcases/Buttons';
-// import ToastsShowcase from './showcases/Toasts';
-
 
 type Props = {
   children: ReactNode;
 };
-
-
 
 const Layout: React.FC<Props> = (props) => (
   <div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,22 +2,56 @@ import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.css'
 import './style.css'; 
-
-import TagManager from "react-gtm-module";
-import { useEffect } from 'react';
-
+import { useEffect } from 'react'
+import Script from 'next/script'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
 const App = ({ Component, pageProps }: AppProps) => {
 
+  const router = useRouter()
   useEffect(() => {
-    TagManager.initialize({ gtmId: 'GTM-K9MS49R' });
-  }, []);
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
+    </>
   );
 };
 
-export default App;
+export default App
+
+
+
+
+
+
